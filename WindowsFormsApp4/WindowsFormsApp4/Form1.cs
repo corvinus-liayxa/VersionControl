138b0b5e2cf330339158018ff42a55acfaa31d11
LIAYXA <vajk.koszegi@stud.uni-corvinus.hu>
2021-11-12 20:56:39 +0100

    Átnevezés

--- WindowsFormsApp4/WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -7,50 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp4.Abstractions;
 using WindowsFormsApp4.Entities;
 
 namespace WindowsFormsApp4
 {
+
     
 
 
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
-        private BallFactory Factory { get; set; }
+        private List<Toy> _toys = new List<Toy>();
+        private Interface1 _factory;
+        private Interface1 Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
 
         public Form1()
         {
             InitializeComponent();
             Factory = new BallFactory();
-
-
-        
         }
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add(ball);
-            ball.Left = -ball.Width;
-            panel1.Controls.Add(ball);
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            panel1.Controls.Add(toy);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var toy in _toys)
             {
-                ball.MoveToy();
-                if (ball.Left > maxPosition)
-                    maxPosition = ball.Left;
+                toy.MoveToy();
+                if (toy.Left > maxPosition)
+                    maxPosition = toy.Left;
             }
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
-                panel1.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                var oldesttoy = _toys[0];
+                panel1.Controls.Remove(oldesttoy);
+                _toys.Remove(oldesttoy);
             }
         }
     }
