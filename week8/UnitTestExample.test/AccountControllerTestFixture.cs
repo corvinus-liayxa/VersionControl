bab0ec5af742cd320e2a908da23f2bbec598d594
LIAYXA <vajk.koszegi@stud.uni-corvinus.hu>
2021-11-26 16:00:50 +0100

    3A

--- week8/UnitTestExample.test/AccountControllerTestFixture.cs
+++ week8/UnitTestExample.test/AccountControllerTestFixture.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.test
 {
@@ -12,7 +13,11 @@ namespace UnitTestExample.test
         [Test]
         private void TestValidateEmail (string email, bool expectedResult)
         {
+            var accountController = new AccountController();
 
+            var actualResults = accountController.ValidateEmail(email);
+
+            Assert.AreEqual(expectedResult, actualResults);
 
         }
     }
