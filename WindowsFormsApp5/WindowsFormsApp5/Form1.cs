9152ff7f39b6b960b411168b58a96db2e44da030
LIAYXA <vajk.koszegi@stud.uni-corvinus.hu>
2021-12-03 10:29:53 +0100

    szimulacio

--- WindowsFormsApp5/WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -18,6 +18,8 @@ namespace WindowsFormsApp5
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
+        Random rng = new Random(1234);
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +27,25 @@ namespace WindowsFormsApp5
             Population = GetPeople(@"C:\Temp\nép.csv");
             BirthProbabilities = szuletes(@"C:\Temp\születés.csv");
             DeathProbabilities = halal(@"C:\Temp\halál.csv");
+
+            // Végigmegyünk a vizsgált éveken
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         public List<Person> GetPeople (string csvpath)
