9d4a50d91dd8ed16fbf6c5d863b42d4616447f1d
LIAYXA <vajk.koszegi@stud.uni-corvinus.hu>
2021-12-07 15:45:58 +0100

    generacio

--- WindowsFormsApp6/WindowsFormsApp6/Form1.cs
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -21,19 +21,55 @@ namespace WindowsFormsApp6
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
+
         public Form1()
         {
             InitializeComponent();
 
+
+
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            gc.GameOver += Gc_GameOver;
 
             for (int i = 0; i < populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
+
+            }
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.Start();
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
             }
             gc.Start();
-            
+
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
         }
     }
 }
